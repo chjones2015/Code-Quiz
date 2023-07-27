81015e0001cd614ae12ac2e05488b4dfb57499c0
Christian Jones <chjones2015@gmail.com>
2023-07-26 21:49:12 -0500

    added some html and javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+var questions = [
+    {
+      question: "What is the result of the following expression: 5 + 5?",
+      choices: ["55", "10", "555", "5"],
+      answer: "10",
+    },
+    {
+      question: "What is the output of console.log(typeof null);?",
+      choices: ["object", "null", "undefined", "string"],
+      answer: "object",
+    },
+    {
+      question: "What is the output of console.log('hello'.charAt(0));?",
+      choices: ["h", "e", "1", "o"],
+      answer: "h",
+    },
+  ];
+  
